10f255a932657e4b6ebe46c0bb1ec2774b69f949
adrianigl24 <uo209738@uniovi.es>
2016-02-16 21:58:40 +0100

    Integracion con la capa de  servicio para validar el usuario

--- src/main/java/es/uniovi/asw/controller/MainController.java
+++ src/main/java/es/uniovi/asw/controller/MainController.java
@@ -1,35 +1,47 @@
 package es.uniovi.asw.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.uniovi.asw.model.UserInfo;
+import es.uniovi.asw.service.UserService;
 
 @RestController
 public class MainController {
 	private final String OK = "OK";
 	private final String KO = "FALLO";
 
+	@Autowired
+	private UserService userService;
+
 	@RequestMapping("/")
 	public String landing() {
 		return "Voters 2a Service";
 	}
 
+	/*
+	 * { "email":"a@a.com", "password": "13" }
+	 */
 	@RequestMapping(value = "/validar", method = RequestMethod.POST)
 	public String validar(@RequestBody UserInfo user) {
 		if (user == null) {
 			return this.KO;
 		}
-		
+
 		// TODO: validar que email y clave no sean nulos ni vacios
-		
-		System.out.println("if");
-		if(user.getEmail().equals("a@a.com") && user.getPassword().equals("123")) {
+
+		// if(user.getEmail().equals("a@a.com") &&
+		// user.getPassword().equals("123")) {
+		// return this.OK;
+		// }
+
+		if (this.userService.validar(user)) {
 			return this.OK;
 		}
-	
+
 		return this.KO;
 	}
 
