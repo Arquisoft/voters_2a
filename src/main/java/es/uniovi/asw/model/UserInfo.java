b1f1d8d1cb4fcaf9cf8ae9507d07795cbcd016f2
daniarango94 <daniarango94@gmail.com>
2016-02-17 10:16:55 +0100

    Ampliada clase UserInfo con etiquetas de persistencia

--- src/main/java/es/uniovi/asw/model/UserInfo.java
+++ src/main/java/es/uniovi/asw/model/UserInfo.java
@@ -1,18 +1,26 @@
 package es.uniovi.asw.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.jpa.domain.AbstractPersistable;
 
-public class UserInfo {
+@Entity
+@Table(name = "user")
+public class UserInfo extends AbstractPersistable<Long> {
+
+	private static final long serialVersionUID = 1L;
 
 	private static final Logger log = LoggerFactory.getLogger(UserInfo.class);
 
+	@Column(nullable = false, unique = true)
 	private String email;
+
+	@Column(nullable = false)
 	private String password;
-
-	public UserInfo() {
-
-	}
 
 	public UserInfo(String email, String password) {
 		log.info("Creating user " + email + ". password: " + password);
--- src/main/java/es/uniovi/asw/service/UserService.java
+++ src/main/java/es/uniovi/asw/service/UserService.java
@@ -2,11 +2,9 @@ package es.uniovi.asw.service;
 
 import javax.transaction.Transactional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import es.uniovi.asw.model.UserInfo;
-import es.uniovi.asw.repository.UserRepository;
 
 @Service
 @Transactional
