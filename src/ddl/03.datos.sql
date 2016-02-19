ae9f0409bdefc76aedb47906f1eb86d9fa4768b7
daniarango94 <daniarango94@gmail.com>
2016-02-19 10:30:21 +0100

    Update 03.datos.sql

--- src/ddl/03.datos.sql
+++ src/ddl/03.datos.sql
@@ -1,1 +1,3 @@
-insert into user (id, email, password) values (1, "adrian@gmail.com", "123456");
+insert into user (id, nombre,email,nif,codigo_mesa,password) values (1,"Adrián", "adrian@gmail.com","88888888P", 3,"123456",);
+insert into user (id, nombre,email,nif,codigo_mesa,password) values (1,"Adrián", "adrian@gmail.com","88888888P", 3,"123456",);
+insert into user (id, nombre,email,nif,codigo_mesa,password) values (1,"Adrián", "adrian@gmail.com","88888888P", 3,"123456",);
