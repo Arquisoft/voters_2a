ac0fbbb2e6cc4872b53be7f59f57ce9005ae9b87
edugarcia22 <edugm192@gmail.com>
2016-02-19 16:19:46 +0100

    Modificar el table.sql

--- src/ddl/02.table.sql
+++ src/ddl/02.table.sql
@@ -1,6 +1,9 @@
 CREATE TABLE `voters`.`user` (
   `id` INT NOT NULL,
+  `nombre` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
+  `nif` VARCHAR(255) NOT NULL,
+  `codigo_mesa` VARCHAR(255) NOT NULL,
   `password` VARCHAR(10) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC));
