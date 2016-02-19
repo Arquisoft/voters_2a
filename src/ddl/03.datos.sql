230e7e2e58b63cfa9f9ca53af9df246cb7f678b2
daniarango94 <daniarango94@gmail.com>
2016-02-19 10:32:27 +0100

    Estructura base de las filas de la base de datos

--- src/ddl/03.datos.sql
+++ src/ddl/03.datos.sql
@@ -1,3 +1,2 @@
 insert into user (id, nombre,email,nif,codigo_mesa,password) values (1,"Adrián", "adrian@gmail.com","88888888P", 3,"123456",);
-insert into user (id, nombre,email,nif,codigo_mesa,password) values (1,"Adrián", "adrian@gmail.com","88888888P", 3,"123456",);
-insert into user (id, nombre,email,nif,codigo_mesa,password) values (1,"Adrián", "adrian@gmail.com","88888888P", 3,"123456",);
+
