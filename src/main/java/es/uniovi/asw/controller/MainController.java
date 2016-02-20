010491fa9dd47e8a14515225158adbf3ca064716
adrianigl24 <uo209738@uniovi.es>
2016-02-20 13:23:49 +0100

    crear un servicio para la clave actualizacion del GetVoterInfo

--- src/main/java/es/uniovi/asw/controller/MainController.java
+++ src/main/java/es/uniovi/asw/controller/MainController.java
@@ -1,21 +1,27 @@
 package es.uniovi.asw.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.uniovi.asw.model.UserInfo;
-import es.uniovi.asw.service.UserServiceImpl;
+import es.uniovi.asw.rest.LoginRest;
+import es.uniovi.asw.rest.UserInfoRest;
+import es.uniovi.asw.service.UserService;
+import es.uniovi.asw.util.StringUtil;
 
 @RestController
 public class MainController {
-	private final String OK = "OK";
-	private final String KO = "FALLO";
+	private static final Logger log = LoggerFactory.getLogger(MainController.class);
 
 	@Autowired
-	private UserServiceImpl userService;
+	private UserService userService;
 
 	@RequestMapping("/")
 	public String landing() {
@@ -23,17 +29,56 @@ public class MainController {
 	}
 
 	/*
-	 * { "email":"a@a.com", "password": "13" }
+	 * {"login": "email", "password": "password"}
 	 */
-	@RequestMapping(value = "/validar", method = RequestMethod.POST)
-	public String validar(@RequestBody UserInfo user) {
-		if (user == null)
-			return this.KO;
+	@RequestMapping(value = "/GetVoterInfo", method = RequestMethod.POST)
+	public @ResponseBody UserInfoRest validar(@RequestBody LoginRest login) {
+		if (login == null) {
+			throw new ResourceNotFoundException();
+		}
 
-		else if (this.userService.validar(user))
-			return this.OK;
+		try {
+			UserInfo u = new UserInfo(login.getLogin(), login.getPassword());
+			UserInfo valido = this.userService.validar(u);
 
-		return this.KO;
+			if (valido != null) {
+				return new UserInfoRest(valido.getNombre(), valido.getNif(), valido.getEmail(), valido.getCodigoMesa());
+			}
+
+		} catch (Exception e) {
+			log.error("Se produjo un error en la llamada a /GetVoterInfo. " + e.getMessage());
+			throw new ResourceNotFoundException();
+		}
+
+		throw new ResourceNotFoundException();
+	}
+
+	/*
+	 * {"login": email, "password": password}
+	 */
+	@RequestMapping(value = "/UpdatePassword", method = RequestMethod.POST)
+	public String cambiarClave(@RequestBody LoginRest login) {
+		if (login == null) {
+			throw new ResourceNotFoundException();
+		}
+
+		try {
+			UserInfo u = new UserInfo(login.getLogin(), login.getPassword());
+			UserInfo valido = this.userService.validar(u);
+
+			if (valido != null) {
+				String pass = StringUtil.getCadenaAlfanumAleatoria(8);
+				valido.setPassword(pass);
+				valido = this.userService.update(valido);
+				return pass;
+			}
+
+		} catch (Exception e) {
+			log.error("Se produjo un error en la llamada a /UpdatePassword. " + e.getMessage());
+			throw new ResourceNotFoundException();
+		}
+
+		throw new ResourceNotFoundException();
 	}
 
 }
