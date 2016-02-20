f14b1497a3a949ad0e2744345b98293f6259234f
adrianigl24 <uo209738@uniovi.es>
2016-02-20 13:21:27 +0100

    crear metodo auxiliar para generar contraseñas aleatorias

---
+++ src/main/java/es/uniovi/asw/util/StringUtil.java
@@ -0,0 +1,25 @@
+package es.uniovi.asw.util;
+
+import java.util.Random;
+
+public final class StringUtil {
+
+	private StringUtil() {
+
+	}
+
+	public static String getCadenaAlfanumAleatoria(int longitud) {
+		String cadenaAleatoria = "";
+		long milis = new java.util.GregorianCalendar().getTimeInMillis();
+		Random r = new Random(milis);
+		int i = 0;
+		while (i < longitud) {
+			char c = (char) r.nextInt(255);
+			if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z')) {
+				cadenaAleatoria += c;
+				i++;
+			}
+		}
+		return cadenaAleatoria;
+	}
+}
